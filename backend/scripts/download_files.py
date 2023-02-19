78382e80159e6ab3c9a9f7c22b4d611cf77f62ea
Gus Beringer <gusberinger@gmail.com>
2023-02-18 19:21:12 -0600

    filter basics file in downloads

--- backend/scripts/download_files.py
+++ backend/scripts/download_files.py
@@ -1,9 +1,35 @@
 import asyncio
 import httpx
 from pathlib import Path
+import gzip
+import csv
+from tqdm import tqdm
 
 DOWNLOAD_FOLDER = Path(__file__).parent / "downloads"
 DOWNLOAD_FOLDER.mkdir(exist_ok=True)
+BASICS_FILEPATH = DOWNLOAD_FOLDER / "title.basics.tsv.gz"
+
+
+
+def filter_basiscs_file():
+    """
+    Reads the basics file and filters it to only include tvSeries and tvEpisode
+    Save the filtered file to title.basics.filtered.tsv.gz
+    """
+    with gzip.open(BASICS_FILEPATH, "rt") as f:
+        reader = csv.DictReader(f, delimiter="\t")
+        filtered_rows = []
+        for row in tqdm(reader, total = 9624114):
+            if row["titleType"] in ["tvSeries", "tvEpisode"]:
+                filtered_rows.append(row)
+
+    print("Filtered rows: ", len(filtered_rows))
+    with gzip.open(DOWNLOAD_FOLDER / "title.basics.filtered.tsv.gz", "wt") as f:
+        writer = csv.DictWriter(f, fieldnames=reader.fieldnames, delimiter="\t")
+        writer.writeheader()
+        writer.writerows(filtered_rows)
+
+
 
 async def download_file(url: str) -> None:
     async with httpx.AsyncClient() as client:
@@ -13,6 +39,14 @@ async def download_file(url: str) -> None:
         with open(file_path, "wb") as f:
             f.write(response.content)
             print(f"Downloaded {file_name}")
+
+
+        if "basics" in file_name:
+            print("Filtering basics file")
+            filter_basiscs_file()
+            print("Done filtering basics file")
+
+
 
 
 async def main():
