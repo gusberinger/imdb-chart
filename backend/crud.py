a38a7b6ee24a61218ffa19e5e9b99903c0ed6bf7
Gus Beringer <gusberinger@gmail.com>
2023-02-21 20:01:15 -0600

    implemented search crud

--- backend/crud.py
+++ backend/crud.py
@@ -1,16 +1,24 @@
 from sqlalchemy.orm import Session
-import models, schemas
+from sqlalchemy import text
+import models
 
 def get_episode(db: Session, parent_tconst: str):
     """
     Returns all episodes matching the parent_tconst, ordered by season_number and episode_number
     """
-    return db.query(models.Episode).filter(models.Episode.parent_tconst == parent_tconst).order_by(models.Episode.season_number, models.Episode.episode_number).all()
+    return db.query(models.EpisodeTable).filter(models.EpisodeTable.parent_tconst == parent_tconst).order_by(models.EpisodeTable.season_number, models.EpisodeTable.episode_number).all()
 
 
-def get_episodes(db: Session):
+def search(db: Session, query: str):
     """
-    Returns all episodes
+    Performs a search for the query using Full Text Search
     """
-    return db.query(models.Episode).all().limit(100)
-
+    sql_query = """
+    SELECT tconst, primary_title, start_year, end_year, ts_rank(primary_title_vector, plainto_tsquery(:query)) as rank
+    FROM search
+    WHERE primary_title_vector @@ to_tsquery(:query)
+    ORDER BY rank, num_votes DESC
+    LIMIT 10;
+    """
+    return db.execute(text(sql_query), {"query": query}).fetchall()
+    
