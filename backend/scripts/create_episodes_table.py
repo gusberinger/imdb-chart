359c48cd3d3e005254e1f090e61a1ed577cde5ca
Gus Beringer <gusberinger@gmail.com>
2023-02-21 18:33:07 -0600

    modified types on create episodes

--- backend/scripts/create_episodes_table.py
+++ backend/scripts/create_episodes_table.py
@@ -25,8 +25,8 @@ from sqlalchemy import (
 
 class EpisodeIndexDict(TypedDict):
     parent_tconst: str
-    season_number: str
-    episode_number: str
+    season_number: int
+    episode_number: int
 
 
 class RatingsDict(TypedDict):
@@ -36,23 +36,34 @@ class RatingsDict(TypedDict):
 
 class EpisodeBasicsDict(TypedDict):
     primary_title: str | None
+    title_type: str
     start_year: int | None
     end_year: int | None
 
 
-class CompleteDict(TypedDict):
+class EpisodesTableDict(TypedDict):
     tconst: str
     parent_tconst: str | None
-    season_number: str | None
-    episode_number: str | None
-    average_rating: float | None
-    num_votes: int | None
+    season_number: int
+    episode_number: int
+    average_rating: float
+    num_votes: int
     primary_title: str | None
     start_year: int | None
     end_year: int | None
 
 
-def create_ratings_dict() -> Dict[str, RatingsDict]:
+class SearchTableDict(TypedDict):
+    tconst: str
+    primary_title: str | None
+    num_votes: int
+    average_rating: float
+    start_year: int | None
+    end_year: int | None
+    num_votes: int | None
+
+
+def read_ratings_file() -> Dict[str, RatingsDict]:
     """
     Reads the ratings file and creates a dictionary of tconst to average rating and number of votes
     """
@@ -73,7 +84,7 @@ def create_ratings_dict() -> Dict[str, RatingsDict]:
     return ratings_dict
 
 
-def create_episodes_index_dict() -> Dict[str, EpisodeIndexDict]:
+def read_index_file() -> Dict[str, EpisodeIndexDict]:
     """
     Reads the episodes file and creates a dictionary of tconst to parent tconst, season number, episode number
     """
@@ -100,7 +111,7 @@ def create_episodes_index_dict() -> Dict[str, EpisodeIndexDict]:
     return episodes_dict
 
 
-def create_episode_basics_dict() -> Dict[str, EpisodeBasicsDict]:
+def read_basics_file() -> Dict[str, EpisodeBasicsDict]:
     """
     Reads the basics file and creates a dictionary of tconst to primary title, start year, end year
     """
@@ -124,9 +135,12 @@ def create_episode_basics_dict() -> Dict[str, EpisodeBasicsDict]:
             end_year = int(end_year) if end_year != "\\N" else None
             primary_title = str(primary_title) if primary_title != "\\N" else None
 
+            title_type = row["titleType"]
+
             basics_dict[tconst] = EpisodeBasicsDict(
                 {
                     "primary_title": primary_title,
+                    "title_type": title_type,
                     "start_year": start_year,
                     "end_year": end_year,
                 }
@@ -134,15 +148,15 @@ def create_episode_basics_dict() -> Dict[str, EpisodeBasicsDict]:
     return basics_dict
 
 
-def create_complete_list() -> list[CompleteDict]:
+def create_episode_list() -> list[EpisodesTableDict]:
     """
     Reads the ratings, episodes and basics files and creates a dictionary of tconst to all the information
     """
-    ratings = create_ratings_dict()
-    basics = create_episode_basics_dict()
-    index = create_episodes_index_dict()
+    ratings = read_ratings_file()
+    basics = read_basics_file()
+    index = read_index_file()
 
-    complete_list: list[CompleteDict] = []
+    complete_list: list[EpisodesTableDict] = []
 
     for tconst, basics_data in tqdm(basics.items()):
         if tconst not in ratings or tconst not in index:
@@ -167,7 +181,35 @@ def create_complete_list() -> list[CompleteDict]:
     return complete_list
 
 
-def create_table(engine: Engine) -> Table:
+def create_search_list() -> list[SearchTableDict]:
+    """
+    Reads the ratings, episodes and basics files and creates a dictionary of tconst to all the information
+    """
+    ratings = read_ratings_file()
+    basics = read_basics_file()
+
+    complete_list: list[SearchTableDict] = []
+
+    for tconst, basics_data in tqdm(basics.items()):
+        if tconst not in ratings:
+            continue
+        ratings_data = ratings[tconst]
+
+        complete_list.append(
+            {
+                "tconst": tconst,
+                "primary_title": basics_data["primary_title"],
+                "start_year": basics_data["start_year"],
+                "end_year": basics_data["end_year"],
+                "average_rating": ratings_data["average_rating"],
+                "num_votes": ratings_data["num_votes"],
+            }
+        )
+
+    return complete_list
+
+
+def create_episodes_table(engine: Engine) -> Table:
     """
     Creates the table in the database if it doesn't exist
     """
@@ -190,18 +232,41 @@ def create_table(engine: Engine) -> Table:
     return table
 
 
-def write_to_table(complete_dicts: list[CompleteDict], engine: Engine, table: Table):
+def create_search_table(engine: Engine) -> Table:
+    """
+    Creates the table for the search engine in the database if it doesn't exist
+    """
+    meta = MetaData()
+    table = Table(
+        "search",
+        meta,
+        Column("tconst", String, primary_key=True),
+        Column("average_rating", Float),
+        Column("num_votes", Integer),
+        Column("primary_title", String),
+        Column("start_year", Integer),
+        Column("end_year", Integer),
+        extend_existing=True,
+    )
+    meta.create_all(engine)
+    return table
+
+def write_to_table(inserted_dicts: list[TypedDict], engine: Engine, table: Table):
     """
     Write all the dictionaries to the table `episodes`
     """
     with engine.connect() as con:
-        con.execute(table.insert(), complete_dicts)
+        con.execute(table.insert(), inserted_dicts)
         con.commit()
 
 
 if __name__ == "__main__":
     username = getpass.getuser()
     engine = create_engine(f"postgresql+psycopg2://{username}@localhost/imdb")
-    complete_list = create_complete_list()
-    table = create_table(engine)
-    write_to_table(complete_list, engine, table)
+    episodes_list = create_episode_list()
+
+    episodes_table = create_episodes_table(engine)
+    search_table = create_search_table(engine)
+
+    write_to_table(episodes_list, engine, episodes_table)
+    
