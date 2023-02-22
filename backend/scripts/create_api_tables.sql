b2334e160af2716717a6fa31b2f5d545a141bff3
Gus Beringer <gusberinger@gmail.com>
2023-02-21 23:33:25 -0600

    fixed bug in episode table

--- backend/scripts/create_api_tables.sql
+++ backend/scripts/create_api_tables.sql
@@ -1,30 +1,31 @@
 DROP TABLE episodes;
 DROP TABLE search;
 
-CREATE OR REPLACE FUNCTION replace_chars(input_text text)
-RETURNS text
-AS $$
-BEGIN
-    input_text := replace(input_text, '.', '');
-    input_text := replace(input_text, ':', '');
-    input_text := replace(input_text, '-', '');
-    input_text := replace(input_text, '.', '');
-    input_text := replace(input_text, '', '');
-    input_text := replace(input_text, '&', 'and');
-    RETURN input_text;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE TABLE episodes AS
-    SELECT b.*, r.num_votes, r.average_rating, e.episode_number, e.season_number, e.parent_tconst
+    SELECT
+        b.tconst,
+        b.primary_title,
+        b.start_year,
+        b.end_year,
+        r.num_votes,
+        r.average_rating,
+        e.episode_number,
+        e.season_number,
+        e.parent_tconst
     FROM basics b
     INNER JOIN ratings r ON r.tconst=b.tconst
     INNER JOIN episode_index e ON e.tconst=b.tconst
-    WHERE title_type='tvEpisode'
-    ORDER BY b.tconst, e.season_number, e.episode_number;
+    WHERE b.title_type='tvEpisode';
 
 CREATE TABLE search AS
-    SELECT b.*, r.num_votes, r.average_rating, LOWER(unaccent(primary_title)) AS searchable_title
+    SELECT
+        b.tconst,
+        b.primary_title,
+        b.start_year,
+        b.end_year,
+        r.num_votes,
+        r.average_rating,
+        LOWER(unaccent(primary_title)) AS searchable_title
     FROM basics b
     INNER JOIN ratings r ON r.tconst=b.tconst
     WHERE (title_type='tvSeries' OR title_type='tvMiniSeries')
