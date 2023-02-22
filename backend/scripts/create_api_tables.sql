5ab0ddd5c8de14ec9042e34e2051851fdee55229
Gus Beringer <gusberinger@gmail.com>
2023-02-21 19:15:14 -0600

    add sql statements for necessary api tables

---
+++ backend/scripts/create_api_tables.sql
@@ -0,0 +1,19 @@
+DROP TABLE episodes;
+DROP TABLE search;
+
+CREATE TABLE episodes AS
+    SELECT b.*, r.num_votes, r.average_rating, e.episode_number, e.season_number
+    FROM basics b
+    INNER JOIN ratings r ON r.tconst=b.tconst
+    INNER JOIN episode_index e ON e.tconst=b.tconst
+    WHERE title_type='tvEpisode'
+    ORDER BY b.tconst, e.season_number, e.episode_number;
+
+CREATE TABLE search AS
+    SELECT b.*, r.num_votes, to_tsvector(b.primary_title) AS primary_title_vector
+    FROM basics b
+    INNER JOIN ratings r ON r.tconst=b.tconst
+    WHERE title_type='tvSeries'
+    AND r.num_votes > 100;
+
+CREATE INDEX search_primary_title_vector_idx ON search USING GIN(primary_title_vector);
