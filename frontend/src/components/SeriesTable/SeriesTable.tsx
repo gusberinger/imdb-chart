c0f91267847e8f1d3acfcc817fe02fd354727fb7
Gus Beringer <gusberinger@gmail.com>
2023-03-02 21:50:58 -0600

    simple mui table

--- frontend/src/components/SeriesTable/SeriesTable.tsx
+++ frontend/src/components/SeriesTable/SeriesTable.tsx
@@ -1,48 +1,48 @@
 import React from "react"
 import { useStore } from "../../hooks/store"
-import MaterialTable from "@material-table/core"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
 
 const SeriesTable = () => {
 	const episodes = useStore((state) => state.episodes)
-	const { primary_title } = useStore((state) => state.showInfo)
-	const theme = useStore((state) => state.theme)
-
-	const columns = [
-		{ title: "Title", field: "primary_title" },
-		// { title: "Season", field: "season_number" },
-		// { title: "Episode", field: "episode_number" },
-		{ title: "Rating", field: "average_rating", sortDescFirst: true },
-		{
-			title: "Votes",
-			field: "num_votes",
-			render: (row: EpisodeInfo) => {
-				return row.num_votes.toLocaleString()
-			},
-			sortingFn: "number",
-			sortDescFirst: true,
-			sortUndefined: 1,
-		},
-	]
 
 	return (
-		<MaterialTable
-			data={episodes}
-			columns={columns}
-			title={primary_title}
-			options={{
-				draggable: false,
-				rowStyle: {
-					fontFamily: "Roboto, sans-serif",
-					color: theme === "light" ? "#000" : "#fff",
-					backgroundColor: theme === "light" ? "#fff" : "#2d3338",
-				},
-				headerStyle: {
-					fontFamily: "Roboto, sans-serif",
-					color: theme === "light" ? "#000" : "#fff",
-					backgroundColor: theme === "light" ? "#fff" : "#2d3338",
-				},
-			}}
-		/>
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table" size="small">
+				<TableHead>
+					<TableRow>
+						<TableCell>Episode Title</TableCell>
+						<TableCell align="right">Episode #</TableCell>
+						<TableCell align="right">Designation</TableCell>
+						<TableCell align="right">Air Date</TableCell>
+						<TableCell align="right">Rating</TableCell>
+						<TableCell align="right">Votes</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{episodes.map((episode, idx) => {
+						const designation = `S${episode.season_number}E${episode.episode_number}`
+						return (
+							<TableRow key={episode.tconst} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+								<TableCell component="th" scope="row">
+									{episode.primary_title}
+								</TableCell>
+								<TableCell align="right">{idx + 1}</TableCell>
+								<TableCell align="right">{designation}</TableCell>
+								<TableCell align="right">{episode.air_date?.toLocaleDateString()}</TableCell>
+								<TableCell align="right">{episode.average_rating}</TableCell>
+								<TableCell align="right">{episode.num_votes}</TableCell>
+							</TableRow>
+						)
+					})}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	)
 }
 
